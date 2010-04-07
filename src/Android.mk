5490537a6c3a2a7cd85bbe8adcb889ec48cc4557
root <fjiang4>
2010-04-07 11:58:34 +0800

    Copy the wind river 0329 build libwsbm code to here.
    Change-Id: Ic4cbcec2de408bc7525314da8da21d5f8284c83e

---
+++ src/Android.mk
@@ -0,0 +1,38 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=          \
+   wsbm_driver.c           \
+   wsbm_fencemgr.c         \
+   wsbm_mallocpool.c       \
+   wsbm_manager.c          \
+   wsbm_mm.c           \
+   wsbm_slabpool.c         \
+   wsbm_ttmpool.c          \
+   wsbm_userpool.c
+
+LOCAL_CFLAGS += -DHAVE_CONFIG_H
+
+LOCAL_C_INCLUDES :=            \
+   $(LOCAL_PATH)/../       \
+   $(TOPDIR)kernel/include/drm \
+   $(TARGET_OUT_HEADERS)/libttm
+
+LOCAL_COPY_HEADERS_TO := libwsbm/wsbm
+
+LOCAL_COPY_HEADERS :=          \
+   wsbm_atomic.h           \
+   wsbm_driver.h           \
+   wsbm_fencemgr.h         \
+   wsbm_manager.h          \
+   wsbm_mm.h           \
+   wsbm_pool.h         \
+   wsbm_priv.h         \
+   wsbm_util.h
+
+LOCAL_MODULE:= libwsbm
+
+LOCAL_SHARED_LIBRARIES:= libdrm
+
+include $(BUILD_SHARED_LIBRARY)
