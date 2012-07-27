29378496fbff242824af6099e8334a847418d096
Mark F. Brown <mark.f.brown@intel.com>
2012-07-27 00:49:36 -0400

    REVERTME: Do not build libwsbm without IMG graphics enabled
    BZ: 49093
    
    please disable it for MRFLD
    
    Change-Id: Ie6304ad369d67be9c2b65aa6474c9e1e866ab373
    Signed-off-by: Mark F. Brown <mark.f.brown@intel.com>
    Signed-off-by: Yong Yao <yong.yao@intel.com>
    Reviewed-on: http://android.intel.com:8080/62274
    Tested-by: Brouat, Claude <claude.brouat@intel.com>
    Reviewed-by: Brouat, Claude <claude.brouat@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- src/Android.mk
+++ src/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 
+ifeq ($(ENABLE_IMG_GRAPHICS),true)
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:=          \
    wsbm_driver.c           \
@@ -37,3 +39,5 @@ LOCAL_COPY_HEADERS :=          \
    wsbm_priv.h         \
    wsbm_util.h
 include $(BUILD_COPY_HEADERS)
+
+endif
